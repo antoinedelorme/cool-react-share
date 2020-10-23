e81cda05f4a2f2b80ce7528e0c6ee24afd565ac8
antoinedelorme <antoine.delorme@me.com>
2020-10-23 18:05:00 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import React from "react";
-import "./styles.css";
-
-import { FacebookShareButton, FacebookIcon } from "react-share";
-
-export default function SocialMediaButtons(props) {
-  return (
-    <FacebookShareButton
-      url={"http://www.camperstribe.com"}
-      quote={"CampersTribe - World is yours to explore"}
-      hashtag="#camperstribe"
-    >
-      <FacebookIcon size={36} />
-    </FacebookShareButton>
-  );
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import HelmetMetaData from "./helmetComponent";
+import Popper from "@material-ui/core/Popper";
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  FacebookIcon
+} from "react-share";
+class App extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <HelmetMetaData></HelmetMetaData>
+        <Switch>
+          <Route path="/about" component={AboutUs} />
+          <Route path="/blogs" component={Blog} />
+        </Switch>
+        <Popper className={classes.socialMediaPopper} open={true} transition>
+          <FacebookShareButton
+            url={"http://www.camperstribe.com"}
+            quote={"CampersTribe - World is yours to explore"}
+            hashtag="#camperstribe"
+            className={classes.socialMediaButton}
+          >
+            <FacebookIcon size={36} />
+          </FacebookShareButton>
+          <TwitterShareButton
+            url={"http://www.camperstribe.com"}
+            title={"CampersTribe - World is yours to explore"}
+            hashtag="#camperstribe"
+            className={classes.socialMediaButton}
+          >
+            <TwitterIcon size={36} />
+          </TwitterShareButton>
+          <WhatsappShareButton
+            url={"http://www.camperstribe.com"}
+            title={"CampersTribe - World is yours to explore"}
+            separator=":: "
+          >
+            <WhatsappIcon size={36} />
+          </WhatsappShareButton>
+        </Popper>
+      </div>
+    );
+  }
 }
+export default App;
